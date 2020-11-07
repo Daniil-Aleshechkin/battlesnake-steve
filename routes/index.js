94015b1205ebce8fd72280e091aa11468b072ba0
Daniil-Aleshechkin <45086608+Daniil-Aleshechkin@users.noreply.github.com>
2020-11-07 15:04:11 -0600

    Battlesnake test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* BattleSnake code goes here */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var battlesnakeInfo = {
+    apiversion: '1',
+    author: '',
+    color: '#888888',
+    head: 'default',
+    tail: 'default'
+  }
+  response.status(200).json(battlesnakeInfo)
+
+});
+
+router.get('/', function(req, res, next) {
+  var gameData = request.body
+
+  console.log('START')
+  response.status(200).send('ok')
+});
+
+router.get('/', function(req, res, next) {
+  var gameData = request.body
+
+  var possibleMoves = ['up', 'down', 'left', 'right']
+  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
+
+  console.log('MOVE: ' + move)
+  response.status(200).send({
+    move: move
+  })
+});
+
+router.get('/', function(req, res, next) {
+  var gameData = request.body
+
+  console.log('END')
+  response.status(200).send('ok')
 });
 
 module.exports = router;
